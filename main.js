0e9139f9a4fef96aaffde3ef3e2ca96635af8b67
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-06 13:06:34 +0500

    added

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-"use strict"
+"use strict";
 
+const theme = document.querySelector(".theme");
+const body = document.querySelector("body");
+theme.classList.toggle("active-toggle");
 
-let theme = document.querySelector(".theme")
-let body = document.querySelector("body")
 theme.addEventListener("click", function () {
- theme.classList.toggle("active-toggle")
- if (theme.classList.contains("active-toggle")) {
-  body.classList.add("active-body")
- }else {
-  body.classList.remove("active-body")
- }
- })
-
-
-
- // model connections
-
- let model = document.querySelector(".model-user")
- let modelOpenBtn = document.querySelector("#openModal")
-
- modelOpenBtn.addEventListener("click",function (){
-  model.classList.toggle("active")
- })
- model.addEventListener("click", function (item) {
-  if (item.target.classList.contains("model-user")) {
-    model.classList.remove("active");
+  theme.classList.toggle("active-toggle");
+  if (theme.classList.contains("active-toggle")) {
+    body.classList.add("active-body");
+  } else {
+    body.classList.remove("active-body");
   }
 });
 
- document.querySelector('.xicon').addEventListener ('click',function(){
-  model.style.display="none"
- })
+// model connections
+const modal = document.querySelector(".model-user");
+const modalOpenBtn = document.querySelector("#openModal");
+const modalCloseIcon = document.querySelector(".xicon");
+
+modalOpenBtn.addEventListener("click", function () {
+  modal.classList.add("active");
+});
+
+modalCloseIcon.addEventListener("click", function () {
+  modal.classList.remove("active");
+});
+
+modal.addEventListener("click", function (event) {
+  if (event.target.classList.contains("model-user")) {
+    modal.classList.remove("active");
+  }
+});
