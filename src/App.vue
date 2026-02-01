aa7cb94573510a9665612f9615e368aebe0625d4
azamjonbro <azamjondasturchi11@gmail.com>
2026-02-01 20:55:43 +0500

    added

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ export default {
   data() {
     return {
       // API: 'https://safonon.uz/clients',
-      API: "https://safonon.uz/clients",
+      API: "https://oil.techinfo.uz/clients",
       users: [],
       modalVisible: false,
       selectedUser: null,
